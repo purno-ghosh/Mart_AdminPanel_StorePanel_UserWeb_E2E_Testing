57450bf66e6311da8458f4ac20ad966f97a4de59
Office Laptop <pg@gmail.com>
2023-07-01 18:45:54 +0600

    OFC Lap

--- src/test/java/Add_New_Store/Store_Element.java
+++ src/test/java/Add_New_Store/Store_Element.java
@@ -149,7 +149,8 @@ public class Store_Element {
         Store_Adress.sendKeys("Mirpur 10 6am Tech");
 
         WebElement UploadImg = driver.findElement(By.xpath("//input[@type='file']"));
-        // Home PC  C:\Users\user\Desktop\New CV 24-03-2022\TestingIMG.png
+        // Home PC  C:\Users\\user\Desktop\New CV 24-03-2022\TestingIMG.png
+        // update from ofc laptop
         UploadImg.sendKeys("C:\\Users\\Lenovo\\Desktop\\Product_images\\Food\\Food_1.png");
         Tax_Field.sendKeys("10");
         Thread.sleep(2000);
