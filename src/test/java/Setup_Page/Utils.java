653276980fa45ebef11064a81279521ae97e9c66
Office Laptop <pg@gmail.com>
2023-07-15 16:59:15 +0600

    OFC Lap

--- src/test/java/Setup_Page/Utils.java
+++ src/test/java/Setup_Page/Utils.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
